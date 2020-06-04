928427c10b38715f7a95ae79f9996e6e7808d326
Sumkor <sumkor@foxmail.com>
2020-06-04 21:13:37 +0800

    IOC learn

---
+++ spring-sumkor/src/test/java/com/sumkor/ioc/factory/MyBeanTest.java
@@ -0,0 +1,33 @@
+package com.sumkor.ioc.factory;
+
+import com.sumkor.ioc.context.MyBean;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.support.SimpleInstantiationStrategy;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author Sumkor
+ * @since 2020/6/4
+ */
+public class MyBeanTest {
+
+	@Test
+	public void test() {
+		System.out.println("hello 中文");
+	}
+
+	/**
+	 * @see SimpleInstantiationStrategy#instantiate(org.springframework.beans.factory.support.RootBeanDefinition, java.lang.String, org.springframework.beans.factory.BeanFactory)
+	 */
+	@Test
+	public void createBean() throws ClassNotFoundException, NoSuchMethodException {
+		Class<?> clazz = Class.forName("com.sumkor.ioc.context.MyBean");
+		Constructor<?> constructorToUse = clazz.getDeclaredConstructor();
+		Object object = BeanUtils.instantiateClass(constructorToUse);
+		MyBean myBean = (MyBean) object;
+		myBean.sayHello();
+	}
+
+}
