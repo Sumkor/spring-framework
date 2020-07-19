a39d034e61a68ee4d12d2ba720a6050d32bd4254
Sumkor <Sumkor@foxmail.com>
2020-07-19 20:43:27 +0800

    aop 测试

--- spring-sumkor/src/main/java/com/sumkor/aop/config/AopConfig.java
+++ spring-sumkor/src/main/java/com/sumkor/aop/config/AopConfig.java
@@ -3,21 +3,42 @@ package com.sumkor.aop.config;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author 黄泽滨 【huangzebin@i72.com】
  * @since 2020/7/16
  */
 @Aspect
-@Component
+@Configuration
+@ComponentScan("com.sumkor.aop")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AopConfig {
 
+	/**
+	 * 定义在service包和所有子包里的任意类的任意方法的执行：
+	 */
 	@Pointcut("execution(* com.sumkor.aop.service..*(..))")
 	public void pointCut() {
 	}
 
-	@Before("pointCut()")
+	/**
+	 * getBean得到的代理bean的类型，与this相同，则该bean中的所有方法都会执行
+	 */
+	@Pointcut("this(com.sumkor.aop.service.MyServiceImpl)")
+	public void pointCutThis() {
+	}
+
+	/**
+	 * getBean得到的代理bean的原始类型，与target 相同，则该bean中的所有方法都会执行
+	 */
+	@Pointcut("target(com.sumkor.aop.service.MyServiceImpl)")
+	public void pointCutTarget() {
+	}
+
+	@Before("pointCutTarget()")
 	public void before() {
 		System.out.println("before");
 	}
--- spring-sumkor/src/main/java/com/sumkor/aop/context/MyServiceAOPTest.java
+++ spring-sumkor/src/main/java/com/sumkor/aop/context/MyServiceAOPTest.java
@@ -1,7 +1,8 @@
 package com.sumkor.aop.context;
 
-import com.sumkor.aop.config.AppConfig;
+import com.sumkor.aop.config.AopConfig;
 import com.sumkor.aop.service.MyService;
+import com.sumkor.aop.service.MyServiceImpl;
 import com.sumkor.aop.service.MyServiceNoFace;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -10,10 +11,10 @@ import org.springframework.context.support.AbstractApplicationContext;
  * @author Sumkor
  * @since 2020/7/16
  */
-public class MyServiceTest {
+public class MyServiceAOPTest {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopConfig.class);
 		/**
 		 * 总入口
 		 * @see AbstractApplicationContext#refresh()
@@ -32,8 +33,20 @@ public class MyServiceTest {
 		 *
 		 */
 
+		System.out.println("------------------------");
+
 		MyService myService = context.getBean(MyService.class);
 		myService.say();
+		System.out.println(myService instanceof MyServiceImpl);
+		/**
+		 * 使用jdk动态代理时，返回false
+		 * 使用cglib动态代理时，返回true
+		 *
+		 * jdk动态代理：bean extends Prosy implement MyService
+		 * cglib动态代理：bean extends MyServiceImpl implement MyService
+		 */
+
+		System.out.println("------------------------");
 
 		MyServiceNoFace myServiceNoFace = context.getBean(MyServiceNoFace.class);
 		myServiceNoFace.say();
